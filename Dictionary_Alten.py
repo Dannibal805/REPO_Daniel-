5cb973523fd6ba22dcda68b97f83d229501fb809
Dannibal <adanielsguerrero@hotmail.com>
2026-09-23 12:58:16 -0600

    Agregar ejercicio de diccionario

---
+++ Dictionary_Alten.py
@@ -0,0 +1,20 @@
+logs = ["ERROR", "INFO", "WARN", "ERROR", "INFO", "ERROR"]
+
+a = logs.count("ERROR")
+b = logs.count("WARN")
+c = logs.count("INFO")
+
+D = [a, b, c]
+
+dic = {}
+
+for log in logs:
+    if log in dic:
+        dic[log] += 1
+    else:
+        dic[log] = 1
+
+error_mas_comun = max(dic,key = dic.get)
+dic["Most_Common"] = error_mas_comun
+
+print(dic)
